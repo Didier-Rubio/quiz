71677cb2401fa75c40923c9f732a7d0475eab92e
Didier Rubio <didierubio@hotmail.com>
2015-06-11 08:19:10 -0500

    Depliegue DB en Heroku
    Depliegue DB en Heroku.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,19 @@
 var models = require('../models/models.js');
 
-// GET /Quizes/Question
-exports.question = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
-	});	
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  })
 };
 
-// GET /Quizes/Answer
-exports.answer = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
-		if(req.query.respuesta.toLowerCase() === quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-		}
-	});	
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+    }
+  })
 };
