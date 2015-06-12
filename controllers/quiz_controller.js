d684f555077388b8b17f369ad9f51c977a174eff
Didier Rubio <didierubio@hotmail.com>
2015-06-12 16:29:16 -0500

    Crear Preguntas
    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -34,10 +34,23 @@ exports.show = function(req, res){
 // GET /Quizes/:Id/Answer
 exports.answer = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){	
-		var resultado = 'Incorrecto'
+		var resultado = 'Incorrecto'		
 		if(req.query.respuesta.toLowerCase() === req.quiz.respuesta){
 			resultado = 'Correcto';
 		} 
 		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 	});	
+};
+
+// GET /New
+exports.new = function(req, res){	
+	var quiz = models.Quiz.build({pregunta: 'Pregunta',  respuesta: 'Respuesta'});
+	res.render('quizes/new', { quiz: quiz });	
+};
+
+// POST / quizes / Create
+exports.create = function(req, res){	
+	var quiz = models.Quiz.build( req.body.quiz );
+	quiz.respuesta = quiz.respuesta.toLowerCase();
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){res.redirect('/quizes');});	
 };
