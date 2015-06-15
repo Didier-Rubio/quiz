0f9c713dd3b0cd2f41d1ec971d9492b92935ebd4
Didier Rubio <didierubio@hotmail.com>
2015-06-15 15:40:14 -0500

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW de autorización de accesos a HTTP restingidos
+exports.loginRequired = function(req, res, next){
+	if(req.session.user){
+		next();
+	}else {
+		res.redirect('/login');
+	}
+};
+
 // GET / login / formulario de login
 exports.new = function(req, res){	
 		var errors = req.session.errors || {};
@@ -12,7 +21,7 @@ exports.create = function(req, res){
 		var userController = require('./user_controller');
 		userController.autenticar(login, password, function(error, user){
 			if(error){
-				req.session.errors = [{"message": "Se ha Producido un Error " + error}];
+				req.session.errors = [{"message": "Se ha Producido un Error: " + error}];
 				res.redirect("/login");
 				return;
 			}
@@ -22,7 +31,7 @@ exports.create = function(req, res){
 };
 
 // DELETE /logout  == Destruir la session
-exports.destroy = function(req, res){	
+exports.destroy = function(req, res){		
 		delete req.session.user;
 		res.redirect(req.session.redir.toString());	
 };
